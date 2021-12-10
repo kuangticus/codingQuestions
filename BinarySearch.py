0c6455bd36583629d6a54da3a5b28c95d9f36138
kuangticus <weihaokuang@gmail.com>
2021-12-10 11:30:13 -0700

    Intial commit for coding practice

---
+++ BinarySearch.py
@@ -0,0 +1,29 @@
+def binarySearch(nums, target, left , right):
+
+    while right >= left:
+        # we need to use the pivot for this
+        # pivot that is calculated is got towards the number we are querying
+        pivot = left  + (right-left) // 2
+        print(nums[pivot], end = ',')
+
+        if nums[pivot] == target:
+            return pivot
+        
+        elif target > nums[pivot]:
+            left = pivot+1
+        else:
+            right = pivot-1
+    return -1
+
+
+def main ():
+    # given a acended sorting list define binary search function iteratively and rescursively
+    nums = [1,2,3,4,9,8,19,20]
+    target = 19
+
+    print(nums)
+    l, r = 0, len(nums) - 1
+
+    print(binarySearch(nums, target, l, r))
+
+main()
