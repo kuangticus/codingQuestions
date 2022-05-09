067c6509b50708fe31ad0509783725db58d1be0c
kuangticus <weihaokuang@gmail.com>
2022-05-09 12:11:31 -0600

    pushing updates for make change

--- dailyCodingQs/makeChange.py
+++ dailyCodingQs/makeChange.py
@@ -1,13 +1,12 @@
 
 
- # Using the least amount of coins using American coin system, make n cents
+ # Using the least amount of coins using American Denominations, make n cents
 
  # coins = [1, 5, 10, 25] Normal minted coins in every day use
 
-from threading import local
-
-from pyparsing import makeHTMLTags
-
+#Hard set since tit's only the American system
+#Time Complexity: O(n) one pass through the coin array, technically since it's just for it can be considerd O(1)
+#Space Complexity: O(1) since no new space is created other than to hold one value therefore O(1)
 
 coins = [1,5,10,25]
 
@@ -20,6 +19,7 @@ def makeChange(n):
         if n == 0:
             return minCoins
         
+        # start from largest # and find the max amount needed
         if coins[i] <= n:
             tempCount = n//coins[i]
             minCoins += tempCount
